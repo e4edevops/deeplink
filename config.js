aefd4e9cdee15b9dacb2cc2ce88ad20b5804dba0
e4edevops1 <devops@e4email.net>
2020-02-12 14:02:29 +0000

    build(deeplink): update site

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.appConfig = {
-    defaultLocation: "http://172.93.48.21/",
+    defaultLocation: "http://172.93.48.21:8015",
     bugsnagApiKey: "12345678",
     deeplinkBase: "flyve://register",
     androidDefaultLink: "the route of your application in the marketplace",
